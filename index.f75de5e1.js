6689f359d94f652b5441552b80b350b1edb26d0b
Tania Kolesnik <kolesnik@gmail.com>
2024-12-18 14:09:32 +0000

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const messageWindow = document.createElement("div");
+    const messageTitle = document.createElement("h2");
+    const messageText = document.createElement("p");
+    messageTitle.textContent = title;
+    messageTitle.classList.add("title");
+    messageText.textContent = description;
+    messageWindow.classList.add("notification");
+    messageWindow.style.top = posTop + "px";
+    messageWindow.style.right = posRight + "px";
+    if (type === "success") messageWindow.classList.add("success");
+    if (type === "warning") messageWindow.classList.add("warning");
+    if (type === "error") messageWindow.classList.add("error");
+    messageWindow.append(messageTitle);
+    messageWindow.append(messageText);
+    document.body.append(messageWindow);
+    setTimeout(()=>{
+        messageWindow.style.display = "none";
+    }, "2000");
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
